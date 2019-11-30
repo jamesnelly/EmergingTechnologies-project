633a1406ad24d9fa68e12cef4f2593542c7fa7b9
jamesnelly <g00346996@gmit.ie>
2019-11-30 15:25:14 +0000

    updating reeadme working on flask

--- WebApp/WebApp.py
+++ WebApp/WebApp.py
@@ -1,22 +1,29 @@
-from flask import Flask, jsonify, request, send_from_static
 
-from scipy.misc import imread, imresize
-
-import tensorflow as tf
-from keras.models import load_model
-
-import keras as kr
-import numpy as np
-import io
+from flask import Flask, render_template, request
+import re
+from io import BytesIO
 import base64
+from keras.models import load_model
+import numpy as np
+from PIL import Image, ImageOps
+import cv2
+#import tensorflow as tf
+
+
 
 app = Flask(__name__)
 
-global model, graph
+model = load_model('model/model.h5')
 
-def Get_Model():
-    mod = load_model('../model/model.h5')
-    print('Model loaded')
-    return mod
+imageHeight = 28
+imageWidth = 28
+size = imageHeight, imageWidth
+
+@app.route('/')
+def homePage():
+    return render_template('WebApp/static/DigitRecognizer.html')
 
     
+
+if __name__ == '__main__':
+    app.run(debug=False, threaded=False)
